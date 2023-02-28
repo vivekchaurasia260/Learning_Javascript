41523e045258ac63cf862cf043b8d02ff1e6479c
Sirius <vivekchaurasia260@gmail.com>
2023-02-28 22:56:20 +0530

    Javascript Object Model Folder committed successfully.

---
+++ JS_DOM/Introd_DOM.js
@@ -0,0 +1,6 @@
+/*
+    In this section we will learn about the Document Object Model(DOM).
+    The will allow us to interface our javascript code to interact with html ans CSS.
+    Browsers will construct the DOM, which basically means storing all HTML tags as Javascript objects.
+*/
+
