0908923eabf58d182593a19c17b32ce383bee6ba
Sirius <vivekchaurasia260@gmail.com>
2023-02-28 22:54:29 +0530

    Object Exercise Folder committed successfully.

--- JS_ObjectExercise/script.js
+++ JS_ObjectExercise/script.js
@@ -6,5 +6,45 @@
 var employee = {
     name: "John Smith",
     job: "Programmer",
-    age: 31
+    age: 31,
+    nameLength: function()
+    {
+        console.log(this.name.length);
+    }
 }
+
+employee.nameLength();
+
+////////////////////
+// PROBLEM 2 //////
+//////////////////
+
+//Given the object
+var employee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    report: function()
+    {
+        alert("Name is " + this.name + ", Job is " + this.job + ", Age is " + this.age);
+    }
+}
+
+employee.report();
+
+////////////////////
+// PROBLEM 3 //////
+//////////////////
+
+//Given the object
+var employee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    lastName: function()
+    {
+        var str = console.log(this.name.split(" ")[1]);
+    }
+}
+
+employee.lastName();
